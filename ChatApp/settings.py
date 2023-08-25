4e9a6bfde7a2ef2920989f5d5eb96c235011f862
Aron <aronesadegh@gmail.com>
2023-08-26 03:07:36 +0330

    add google authentication

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -19,8 +19,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
     'django_cleanup',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     # My Apps
     'chat_app.apps.ChatAppConfig',
     'account_app.apps.AccountAppConfig',
@@ -110,3 +115,30 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+# Additional configuration settings
+SOCIALACCOUNT_QUERY_EMAIL = True
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_EMAIL_REQUIRED = True
+
--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat_app.urls')),
     path('account/', include('account_app.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
